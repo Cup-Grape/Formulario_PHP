482515062074c9924d16f809e1e106ab60c5b5c8
Cup-Grape <feliposs11@gmail.com>
2022-04-06 09:05:34 -0300

    Conexão BD
    Feito a conexão com BD, e pagina para cadastro

---
+++ bd_conexao.php
@@ -0,0 +1,36 @@
+<?php
+
+
+class conexao
+{
+    public $con;
+    public $database;
+
+    public function entrarbd()
+    {
+        $user = "root";
+        $password = "456951Fe";
+        $database = "formulario";
+        $hostname = "localhost";
+        $conexao = new conexao;
+        $conexao -> con = $con = mysqli_connect($hostname, $user, $password) or die("erro na conexão");
+        mysqli_select_db($con, $database) or die("erro na conexão com database");
+
+    }
+
+    public function inserts($nome, $email){
+        $obj = new conexao;
+        $comandosql = "INSERT INTO $obj -> database (nome, email) VALUES ('$nome', '$email')";
+        if(mysqli_query($obj->con,$comandosql)){
+            echo 'Cadastro realizado com sucesso!';
+        }
+
+
+
+
+    }
+
+
+
+}
+?>
---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <form action="" method="post">
+
+        <label for="">Nome</label>
+        <input type="text" name="nome">
+        <label for="">email</label>
+        <input type="email" name="email">
+        <input type="button" value="Enviar">
+
+    </form>
+
+
+
+
+</body>
+
+</html>
